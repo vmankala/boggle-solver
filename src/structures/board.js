2366e09522161a108a939aed48db27417499688f
Vidip Mankala <vmankala23@gmail.com>
2020-08-08 18:12:14 -0700

    Add board implementation

---
+++ src/structures/board.js
@@ -0,0 +1,38 @@
+export default class Board {
+    constructor(board) {
+        this.board = board || [
+            ['', '', '', ''],
+            ['', '', '', '']
+            ['', '', '', '']
+            ['', '', '', '']
+        ];
+        
+        // neighbors matrix
+        this.nmatrix = [
+            [-1, -1],
+            [-1, 0],
+            [-1, 1],
+            [0, -1].
+            [0, 1],
+            [1, -1],
+            [1, 0],
+            [1, 1]
+        ];
+    }
+
+    getBoard() {
+        return this.board;
+    }
+
+    checkCell(x, y) {
+        return x >= 0 && x < this.board[0].length && y >= 0 && y < this.board.length && this.board[y][x] !== '';
+    }
+
+    getCell(x, y) {
+        return this.board[y][x];
+    }
+
+    setCell(x, y, c) {
+        this.board[y][x] = c;
+    }
+}
